9d9de6f14bc9454ca7c270fdddd21c575e9d1fa0
“Karan <karanseth200066@gmail.com>
2022-04-08 11:13:46 +0530

    Added First Angular Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-Project-Demo';
+  title = 'My First Angular Task';
 }
